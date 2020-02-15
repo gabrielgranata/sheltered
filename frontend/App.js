a2af86281e5543ff40dc2ef52f24e806bfd82866
Rachel Wong <rachel.wong@flybits.com>
2020-02-15 16:55:42 -0500

    login set initial route

--- frontend/App.js
+++ frontend/App.js
@@ -25,7 +25,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
           name="Home"
           component={Login}
